6f71851eba2db48b499448362b2d1ac4042de190
turquoise-orchid <turquoise.orchid@gmail.com>
2017-06-20 09:46:46 +0300

    element selection on click event added

--- main.js
+++ main.js
@@ -5,6 +5,43 @@ window.onbeforeunload = function () {
 	return 'Do you want?';
 };
 
+var chooseItem = function(elem) {
+	unChooseItem();
+	var item;
+	var items = document.getElementsByClassName('item');
+	if (!items.length) {
+		return;
+	}
+		switch(elem) {
+	case "box2":
+		item = items[0];
+		item.classList.add('selected');
+	break;
+	case "box6":
+		item = items[1];
+		item.classList.add('selected');
+	break;
+	case "box4":
+		item = items[2];
+		item.classList.add('selected');
+	break;
+	}
+
+};
+
+var unChooseItem = function() {
+	var item;
+	var items = document.getElementsByClassName('item');
+	if (!items.length) {
+		return;
+	}
+	for (var i = 0; i < items.length; i++) {
+	item = items[i];
+	item.classList.remove('selected');
+	}
+};
+
+
 var showPopup = function() {
 	var popupDiv;
 	var popupDivs = document.getElementsByClassName('wrap');
